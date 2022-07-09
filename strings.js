f300e4de41188fbda111ef70c27095b867cc373c
TonyB <tonybaromagic@gmail.com>
2022-07-09 17:03:09 -0400

    second revision

--- strings.js
+++ strings.js
@@ -5,3 +5,21 @@ function removeBlanks(string){
 
 console.log(removeBlanks(" Pl ayTha tF u nkyM usi c "))
 console.log(removeBlanks("I can not BELIEVE it's not BUTTER"))
+
+function getDigits(string){
+    var newString=string.split("");
+    var final=[]
+    for (var x=0; x<= newString.length-1; x++){
+        if (isNaN(Number(newString[x]))){    
+        }
+        else{final.push(newString[x])}
+    }
+    return final.join("")
+}
+
+console.log(getDigits("abc8c0d1ngd0j0!8"))
+console.log(getDigits("0s1a3y5w7h9a2t4?6!8?0"))
+
+function acronyms(string){
+    newString= string.split(" ")
+}
