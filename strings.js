ae763d4c9d7ec7c4b8bfe25286e31b73ee867a24
TonyB <tonybaromagic@gmail.com>
2022-07-09 16:44:38 -0400

    start

---
+++ strings.js
@@ -0,0 +1,7 @@
+function removeBlanks(string){
+    var newString=string.split(" ");
+    return newString.join("")
+}
+
+console.log(removeBlanks(" Pl ayTha tF u nkyM usi c "))
+console.log(removeBlanks("I can not BELIEVE it's not BUTTER"))
